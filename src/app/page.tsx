920609a784458f0507757867e9599ad1a9951340
Santosh Joshi <santoshjoshi2003@gmail.com>
2024-11-09 23:28:22 +0530

    added framer-motion, improved all posts UI, using paginations to show pagesmm, improved nav bar to include server and client side menu items just experimental, Add home page inspiration is :https://wp.9d8.dev/

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,89 @@
 "use client"
 
-import { Avatar, Button, Card, CardBody, CardFooter, CardHeader } from "@nextui-org/react";
+import { IconBox, IconFile, IconFolder, IconPencil, IconTag, IconUser } from "@tabler/icons-react";
+import Link from "next/link";
 import React from "react";
 
 
 export default function Home() {
-  const [isFollowed, setIsFollowed] = React.useState(false);
   return (
-    <Card className="max-w-[340px] mx-auto mt-4 max-w-md overflow-auto">
-    <CardHeader className="justify-between">
-      <div className="flex gap-5">
-        <Avatar isBordered radius="full" size="md" src="https://nextui.org/avatars/avatar-1.png" />
-        <div className="flex flex-col gap-1 items-start justify-center">
-          <h4 className="text-small font-semibold leading-none text-default-600">Zoey Lang</h4>
-          <h5 className="text-small tracking-tight text-default-400">@zoeylang</h5>
-        </div>
+    <article className="prose-m-none">
+      
+        
+      <div className="grid md:grid-cols-3 gap-4 mt-6 not-prose p-4">
+        <Link
+          className=" border h-48 bg-green-300/30 rounded-lg p-4 flex flex-col justify-between hover:scale-[1.02] transition-all"
+          href="/posts"
+        >
+          <IconPencil size={32} className="" />
+          <span className="text-center">
+            Posts{" "}
+            <span className="block text-sm text-muted-foreground">
+              All posts from your WordPress
+            </span>
+          </span>
+        </Link>
+        <Link
+          className="border h-48 bg-yellow-900/35 rounded-lg p-4 flex flex-col justify-between hover:scale-[1.02] transition-all"
+          href="/pages"
+        >
+          <IconFile size={32} />
+          <span>
+            Pages{" "}
+            <span className="block text-sm text-muted-foreground">
+              Custom pages from your WordPress
+            </span>
+          </span>
+        </Link>
+        <Link
+          className="border h-48 bg-orange-400/45 rounded-lg p-4 flex flex-col justify-between hover:scale-[1.02] transition-all"
+          href="/posts/authors"
+        >
+          <IconUser size={32} />
+          <span>
+            Authors{" "}
+            <span className="block text-sm text-muted-foreground">
+              List of the authors from your WordPress
+            </span>
+          </span>
+        </Link>
+        <Link
+          className="border h-48 bg-fuchsia-500/50 rounded-lg p-4 flex flex-col justify-between hover:scale-[1.02] transition-all"
+          href="/posts/tags"
+        >
+          <IconTag size={32} />
+          <span>
+            Tags{" "}
+            <span className="block text-sm text-muted-foreground">
+              Content by tags from your WordPress
+            </span>
+          </span>
+        </Link>
+        <Link
+          className="border h-48 bg-teal-100/20 rounded-lg p-4 flex flex-col justify-between hover:scale-[1.02] transition-all"
+          href="/posts/categories"
+        >
+          <IconBox size={32} />
+          <span>
+            Categories{" "}
+            <span className="block text-sm text-muted-foreground">
+              Categories from your WordPress
+            </span>
+          </span>
+        </Link>
+        <a
+          className="border h-48 bg-accent/50 rounded-lg p-4 flex flex-col justify-between hover:scale-[1.02] transition-all"
+          href="#"
+        >
+          <IconFolder size={32} />
+          <span>
+            Documentation{" "}
+            <span className="block text-sm text-muted-foreground">
+              How to use `this template`
+            </span>
+          </span>
+        </a>
       </div>
-      <Button
-        className={isFollowed ? "bg-transparent text-foreground border-default-200" : ""}
-        color="primary"
-        radius="full"
-        size="sm"
-        variant={isFollowed ? "bordered" : "solid"}
-        onPress={() => setIsFollowed(!isFollowed)}
-      >
-        {isFollowed ? "Unfollow" : "Follow"}
-      </Button>
-    </CardHeader>
-    <CardBody className="px-3 py-0 text-small text-default-400">
-      <p>
-        Frontend developer and UI/UX enthusiast. Join me on this coding adventure!
-      </p>
-      <span className="pt-2">
-        #FrontendWithZoey 
-        <span className="py-2" aria-label="computer" role="img">
-          💻
-        </span>
-      </span>
-    </CardBody>
-    <CardFooter className="gap-10">
-      <div className="flex gap-2">
-        <p className="font-semibold text-default-400 text-small">4</p>
-        <p className=" text-default-400 text-small">Following</p>
-      </div>
-      <div className="flex gap-2">
-        <p className="font-semibold text-default-400 text-small">97.1K</p>
-        <p className="text-default-400 text-small">Followers</p>
-      </div>
-    </CardFooter>
-  </Card>
+    </article>
   );
 }
